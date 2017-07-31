627705458405681126fbef1b850a1016f8730506
J Fixby <github@jfixby.com>
2017-07-31 14:30:52 +0200

    comment

--- project_d/lib/class_d_depends_on_b.dart
+++ project_d/lib/class_d_depends_on_b.dart
@@ -1,4 +1,4 @@
-import 'package:project_a/folderB1/class_b_file.dart'; // manual import
+import 'package:project_a/folderB1/class_b_file.dart'; // manual import works, suggestions - no
 
 class D {
   B instanceOfB = new B();
